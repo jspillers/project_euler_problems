4b1dc2589e7b60244f931242c3f6f28c8cc49d0d
Jrod <jarrod@jtms.net>
2012-04-06 17:54:31 -0400

    adding problem description to each solution file

--- euler_01.rb
+++ euler_01.rb
@@ -1,1 +1,3 @@
+# 01 - Add all the natural numbers below one thousand that are multiples of 3 or 5.
+
 puts (1..999).inject(0) { |sum,x| sum + (x % 3 == 0 || x % 5 == 0 ? x : 0) }.to_s
--- euler_02.rb
+++ euler_02.rb
@@ -1,3 +1,6 @@
+# By considering the terms in the Fibonacci sequence whose values do not exceed
+# four million, find the sum of the even-valued terms.
+
 def fib_sequence_to(limit, sequence=[0,1])
 
   sequence << sequence[-1] + sequence[-2]
